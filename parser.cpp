9713b749c4dcef20d82946e11fdcb6b5d65ce5b8
Abdelrahman-Bishr <abdelrahman.bishr@gmail.com>
2019-12-20 13:37:12 +0200

    Parser works , scanner good but not used yet , overlapping nodes in graphing

--- parser.cpp
+++ parser.cpp
@@ -14,7 +14,7 @@ void Parser::parse()
     input.clear();
     errors=0;
     string s,h;
-    QString path= QDir::currentPath()+"/../parsertest.txt";
+    QString path= QDir::currentPath()+"/../parser_gui/parsertest.txt";
     qDebug()<<path;
     {
 //        freopen(path.toStdString().c_str(),"r",stdin);
